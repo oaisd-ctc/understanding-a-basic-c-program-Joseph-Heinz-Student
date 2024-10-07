6b9f3dafee17f94f8db71a9e8f13e5bee7518bbd
Joseph Heinz <joheb41@student.oaisd.org>
2024-10-07 12:57:17 +0000

    added code needed for assignment

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My Name is Joseph Heinz");
+
+        int age = 16; 
+        string greeting = "Hello World";
+        string firstname = "Joseph";
+        string lastname = "Heinz";
+        double money = 100.09;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent" + money + " on an Energy Drink.");
+    }
+}
